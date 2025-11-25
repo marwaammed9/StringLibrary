81aeb76e66fc9c978ce6af35a5a28e8ac2d46c41
marwaammed9 <144134875+marwaammed9@users.noreply.github.com>
2025-11-25 16:13:38 +0200

    Add files via upload

---
+++ clsString/clsString/clsString.cpp
@@ -0,0 +1,15 @@
+// clsString.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#include "clsStr.h"
+
+
+int main()
+{
+    clsStr str1("marwa");
+    cout<<str1.CountSmallLetters();
+    
+}
+
